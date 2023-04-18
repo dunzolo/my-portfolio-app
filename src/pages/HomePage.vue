6128f436cf18650b63b7fd49b8eba7f0dd1c2def
dunzolo <rossidavide108@gmail.com>
2023-04-18 15:07:55 +0200

    added first style to home page - import font

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,15 +6,33 @@ export default {
 
 <template lang="">
     <div class="container">
-        <p>Ciao, mi chiamo</p>
-        <h1>Davide Rossi.</h1>
-        <p>Sono un Jr Full Stack Web Developer</p>
+        <p class="greeting">Ciao, mi chiamo</p>
+        <h1 class="name">Davide Rossi.</h1>
+        <p class="description">Sono un Jr Full Stack Web Developer</p>
     </div>
 </template>
 
 <style lang="scss" scoped>
+@use '../style/partials/variables'as *;
+@use '../style/partials/mixins'as *;
     .container{
         width: 90%;
+        height: calc(100vh - 80px);
         margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: center;
+
+        .greeting, .description{
+            font-family: 'Barlow Semi Condensed';
+            font-size: 2rem;
+
+        }
+        .name{
+            font-family: 'Barlow Semi Condensed';
+            color: $secondary_color;
+            font-size: 5rem;
+        }
     }
 </style>
