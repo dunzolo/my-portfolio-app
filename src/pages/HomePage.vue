b9f34d0ff8aa009f18b443f5c477b0b9cdba459c
dunzolo <rossidavide108@gmail.com>
2023-04-24 12:27:10 +0200

    ➡️initialize section projects

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -125,6 +125,36 @@ export default {
                 </div>
             </div>
         </section>
+
+        <!-- projects -->
+        <section id="projects">
+            <div class="container p-0">
+                <h2 class="greeting pb-3"><span class="index">03.</span> Progetti</h2>
+                <div class="row">
+                    <div class="col-lg-4 col-lg-d-flex col-md-12 p-md-0 project-caption">
+                        <h3>Netflix</h3>
+                        <div class="caption">
+                            <p class="">
+                                Replica del famoso sito Netflix utilizzando una chiamata API che ci permette di visualizzare i vari contenuti
+                            </p>
+                            <ul class="ps-4">
+                                <li>funzionalità: ricerca dei titoli, visualizzazione dettagli</li>
+                            </ul>
+                        </div>
+                        <ul class="tech-list">
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>Boostrap</li>
+                            <li>Vue Js</li>
+                            <li>Javascript</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-8 col-md-12 project-image">
+                        <img src="/public/my-portfolio-boolflix.jpg" alt="boolflix">
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -133,7 +163,8 @@ export default {
 @use '../style/partials/mixins' as *;
 
 #about-me,
-#experience {
+#experience,
+#projects {
     padding: 5rem 0;
 }
 
@@ -181,6 +212,43 @@ export default {
 
                 filter: none;
             }
+        }
+    }
+
+    .project-image {
+        img {
+            mix-blend-mode: multiply;
+            filter: grayscale(100%);
+
+
+            width: 100%;
+
+            &:hover {
+                filter: none;
+            }
+        }
+    }
+
+    .project-caption {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .caption {
+        z-index: 1;
+        width: 120%;
+        background-color: $primary_color;
+        padding: 1rem;
+        font-size: 15px;
+    }
+
+    .tech-list {
+        list-style-type: none;
+        display: flex;
+
+        li {
+            padding: 0 10px 10px 0;
         }
     }
 
